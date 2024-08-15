8b1885141f4c8721e24641be2f15b13d7b529392
Matvey Nekrasov <matvey1234567@yandex.ru>
2024-08-15 21:49:20 +0300

    feat: add some api error checks

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,37 @@
 import { TSale } from '@/utils/types';
 import { BASE_URL, mockSalesListUrl } from './constants';
 
+const fetchData = async <T>(url: string): Promise<T> => {
+	const res = await fetch(url);
+	if (res.ok) {
+		return checkResponse<T>(res);
+	} else {
+		throw new Error(`Failed to fetch data from ${url}`);
+	}
+};
+
 const checkResponse = <T>(res: Response): Promise<T> =>
 	res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 
-export const getSalesApi = () =>
-	fetch(`${BASE_URL}/items`).then((res) => {
-		if (res.ok) {
-			return checkResponse<TSale[]>(res);
-		} else {
-			return fetch(mockSalesListUrl).then((res) => checkResponse<TSale[]>(res));
+const handleFetchError = (error: unknown): void => {
+	if (error instanceof Error) {
+		console.error('Request failed:', error);
+		if (
+			error instanceof TypeError &&
+			error.message.includes('Failed to fetch')
+		) {
+			console.error('Possible CORS error:', error);
 		}
-	});
+	} else {
+		console.error('Unknown error:', error);
+	}
+};
+
+export const getSalesApi = async (): Promise<TSale[]> => {
+	try {
+		return await fetchData<TSale[]>(`${BASE_URL}/items`);
+	} catch (error) {
+		handleFetchError(error);
+		return await fetchData<TSale[]>(mockSalesListUrl);
+	}
+};
